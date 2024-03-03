0d5df51123a061f71e1925dbc1f3234b36b63740
CipherPixy032 <mollym3gtr@gmail.com>
2024-03-03 15:33:16 +0700

    Commit Update for assignment

--- Buku15.java
+++ Buku15.java
@@ -1,7 +1,9 @@
 public class Buku15 {
     String judul, pengarang;
     int halaman, stok, harga;
+
     public Buku15() {}
+
     public Buku15(String judul, String pengarang, int halaman, int stok, int harga) {
         this.judul = judul;
         this.pengarang = pengarang;
@@ -9,7 +11,6 @@ public class Buku15 {
         this.stok = stok;
         this.harga = harga;
     }
-
     void tampilInformasi() {
         System.out.println("Judul: " + judul);
         System.out.println("Pengarang: " + pengarang);
@@ -17,19 +18,36 @@ public class Buku15 {
         System.out.println("Sisa stok: " + stok);
         System.out.println("Harga: Rp " + harga);
     }
-
     void terjual(int jml) {
-        if (stok > 0) {
+        if (stok >= jml) {
             stok -= jml;
+        } else {
+            System.out.println("Stok tidak cukup untuk penjualan.");
         }
     }
-
     void restock(int jml) {
         stok += jml;
     }
-
     void gantiHarga(int hrg) {
         harga = hrg;
+    }
+    int hitungHargaTotal(int jumlahTerjual) {
+        return harga * jumlahTerjual;
+    }
+    double hitungDiskon(int hargaTotal) {
+        if (hargaTotal > 150000) {
+            return hargaTotal * 0.12;
+        } else if (hargaTotal >= 75000 && hargaTotal <= 150000) {
+            return hargaTotal * 0.05;
+        } else {
+            return 0;
+        }
+    }
+
+    double hitungHargaBayar(int jumlahTerjual) {
+        int hargaTotal = hitungHargaTotal(jumlahTerjual);
+        double diskon = hitungDiskon(hargaTotal);
+        return hargaTotal - diskon;
     }
 }
 
@@ -45,11 +63,23 @@ class BukuMain {
         bk1.terjual(5);
         bk1.gantiHarga(60000);
         bk1.tampilInformasi();
-
+        int hargaTotal = bk1.hitungHargaTotal(5);
+        double diskon = bk1.hitungDiskon(hargaTotal);
+        double hargaBayar = bk1.hitungHargaBayar(5);
+        System.out.println("Harga Total: Rp " + hargaTotal);
+        System.out.println("Diskon: Rp " + diskon);
+        System.out.println("Harga Bayar: Rp " + hargaBayar);
 
         Buku15 bk2 = new Buku15("Self Reward", "Maheera Ayesha", 160, 29, 59000);
         bk2.terjual(11);
         bk2.tampilInformasi();
+        hargaTotal = bk2.hitungHargaTotal(11);
+        diskon = bk2.hitungDiskon(hargaTotal);
+        hargaBayar = bk2.hitungHargaBayar(11);
+        System.out.println("Harga Total: Rp " + hargaTotal);
+        System.out.println("Diskon: Rp " + diskon);
+        System.out.println("Harga Bayar: Rp " + hargaBayar);
+
         Buku15 bookKrisna = new Buku15("The Adventures of Krisna", "Krisna Putra", 250, 50, 80000);
     }
 }
--- DragonGame.java
+++ DragonGame.java
@@ -1,56 +1,72 @@
-class Dragon {
-    int x;
-    int y;
-    int width;
-    int height;
+import java.util.Scanner;
+
+class DragonBody {
+    int x, y, width, height;
+
     void moveLeft() {
+        System.out.println("You moved left");
         x--;
-        if (x < 0) {
-            detectCollision();
-        }
+        printPos();
+        detectCollision(width, height);
     }
+
     void moveRight() {
+        System.out.println("You moved right");
         x++;
-        if (x > width) {
-            detectCollision();
-        }
+        printPos();
+        detectCollision(width, height);
     }
+
     void moveUp() {
+        System.out.println("You moved up");
         y--;
-        if (y < 0) {
-            detectCollision();
-        }
+        printPos();
+        detectCollision(width, height);
     }
+
     void moveDown() {
+        System.out.println("You moved down");
         y++;
-        if (y > height) {
-            detectCollision();
+        printPos();
+        detectCollision(width, height);
+    }
+
+    void printPos() {
+        System.out.println("Current position: " + x + "," + y);
+    }
+
+    void detectCollision(int widthwack, int heightwack) {
+        width = widthwack;
+        height = heightwack;
+        if (x > width || x < 0 || y > height || y < 0) {
+            System.out.println("Game over");
+            System.exit(8);
         }
-    }
-    void printPosition() {
-        System.out.println("Dragon Position: (" + x + ", " + y + ")");
-    }
-    void detectCollision() {
-        System.out.println("Game Over");
-        System.exit(0);
     }
 }
 
-public class DragonGame{
+public class DragonGame {
     public static void main(String[] args) {
-        Dragon dragon = new Dragon();
-        dragon.x = 2;
-        dragon.y = 3;
-        dragon.width = 10;
-        dragon.height = 5;
-        dragon.printPosition();
-        dragon.moveRight();
-        dragon.printPosition();
-        dragon.moveUp();
-        dragon.printPosition();
-        dragon.moveLeft();
-        dragon.printPosition();
-        dragon.moveDown();
-        dragon.printPosition();
+        Scanner inp = new Scanner(System.in);
+        DragonBody dragame = new DragonBody();
+        dragame.height = 5;
+        dragame.width = 5;
+        while (true) {
+            String input = inp.next();
+            switch (input) {
+                case "w":
+                    dragame.moveUp();
+                    break;
+                case "s":
+                    dragame.moveDown();
+                    break;
+                case "a":
+                    dragame.moveLeft();
+                    break;
+                case "d":
+                    dragame.moveRight();
+                    break;
+            }
+        }
     }
 }
